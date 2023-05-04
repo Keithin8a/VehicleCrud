5e460ceead32743d4b753f3626f9eae1af89d7eb
Keith Hirst <keithin8a@gmail.com>
2023-05-04 23:15:20 +0100

    Create validation

--- src/handlers/postVehicle.js
+++ src/handlers/postVehicle.js
@@ -1,20 +1,36 @@
 const postVehicleHandler = async (event) => {
-    const body = JSON.parse(Buffer.from(event.body, 'base64').toString());
+    let validationMessage = "Please check the request body is correct and try again"
 
-    const item = {
-        make: body.make,
-        model: body.model,
-        reg: body.reg,
-        registrationDate: body.registrationDate
-    }
+    try{
+        const body = JSON.parse(Buffer.from(event.body, 'base64').toString());
 
-    return {
-        statusCode: 201,
-        body: JSON.stringify(
-            {
-                data: item
-            }
-        )
+        /* ------ other validation ------
+        Reg is unique
+        assuming no custom plates you would have a regex on the reg
+        reg date is a date
+        */
+
+        const item = {
+            make: body.make,
+            model: body.model,
+            reg: body.reg,
+            registrationDate: body.registrationDate
+        }
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify(
+                {
+                    data: item
+                }
+            )
+        }
+    } catch {
+        return {
+            statusCode: 400,
+            error: "Validation Error",
+            message: validationMessage
+        }
     }
 }
 
--- src/handlers/postVehicle.test.js
+++ src/handlers/postVehicle.test.js
@@ -1,16 +1,15 @@
 const postVehicleHandler = require("./postVehicle");
 
-const input = 'ewogICAgIm1ha2UiOiAidGVzdE1ha2UiLAogICAgIm1vZGVsIjogInRlc3RNb2RlbCIsCiAgICAicmVnIjogIkFBMTFCQkIiLAogICAgInJlZ2lzdHJhdGlvbkRhdGUiOiAiMS8xLzE5OTAiCn0='
-
-const expectedDbResponse = {
-    make: "testMake",
-    model: "testModel",
-    reg: "AA11BBB",
-    registrationDate: "1/1/1990"
-}
-
 describe("postVehicleHandlerTest", () => {
     test('Creates the record in the DB and returns it in the response', async () => {
+        const input = 'ewogICAgIm1ha2UiOiAidGVzdE1ha2UiLAogICAgIm1vZGVsIjogInRlc3RNb2RlbCIsCiAgICAicmVnIjogIkFBMTFCQkIiLAogICAgInJlZ2lzdHJhdGlvbkRhdGUiOiAiMS8xLzE5OTAiCn0='
+        const expectedDbResponse = {
+            make: "testMake",
+            model: "testModel",
+            reg: "AA11BBB",
+            registrationDate: "1/1/1990"
+        }     
+
         const response = await postVehicleHandler({
             body: input
         });
@@ -19,5 +18,17 @@ describe("postVehicleHandlerTest", () => {
 
         expect(response.statusCode).toBe(201);
         expect(dbResponse).toEqual(expectedDbResponse)
+    })
+
+    test('400 status returned with malformed JSON', async () => {
+        const input = 'ewogICAgbWFrZTogInRlc3RNYWtlIiwKICAgICJtb2RlbCI6ICJ0ZXN0TW9kZWwiLAogICAgInJlZyI6ICJBQTExQkJCIiwKICAgICJyZWdpc3RyYXRpb25EYXRlIjogIjEvMS8xOTkwIgp9'
+        
+        const {statusCode, error, message} = await postVehicleHandler({
+            body: input
+        });
+
+        expect(statusCode).toBe(400);
+        expect(error).toBe("Validation Error")
+        expect(message).toBe("Please check the request body is correct and try again")
     })
 })
